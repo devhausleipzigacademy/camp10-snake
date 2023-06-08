b2f812626f603b6fdc9eef46d6b9d278ed14a515
ReallyBoard123 <chirag.prakriya@gmail.com>
2023-06-08 14:48:17 +0200

    draw apple really well

--- src/main.ts
+++ src/main.ts
@@ -1,88 +1,112 @@
-import "./style.css"
-import { coordToId, directionalChange } from "./utils"
+import "./style.css";
+import { coordToId, coordinatesEqual, directionalChange } from "./utils";
 
-export type Coordinate = [number, number]
-export type Id = `${number}-${number}`
-export type Direction = "left" | "right" | "up" | "down"
+export type Coordinate = [number, number];
+export type Id = `${number}-${number}`;
+export type Direction = "left" | "right" | "up" | "down";
 
-const app = document.getElementById("app") as HTMLElement
-const rows = 20
-const columns = 20
+const app = document.getElementById("app") as HTMLElement;
+
+const rows = 20;
+const columns = 20;
+
 const snake: Array<Coordinate> = [
-  [12, 10],
-  [13, 10],
-  [14, 10],
-]
+	[12, 10],
+	[13, 10],
+	[14, 10],
+];
 
-let currentDirection: Direction = "left"
+let apple: Coordinate;
+
+let currentDirection: Direction = "left";
 
 function createBoard() {
-  const board = document.createElement("div")
-  board.classList.add("board")
-  app.appendChild(board)
+	const board = document.createElement("div");
+	board.classList.add("board");
+	app.appendChild(board);
 
-  for (let y = 0; y < rows; y++) {
-    for (let x = 0; x < columns; x++) {
-      const square = document.createElement("div")
-      square.id = `${x}-${y}`
-      board.appendChild(square)
-    }
-  }
+	for (let y = 0; y < rows; y++) {
+		for (let x = 0; x < columns; x++) {
+			const square = document.createElement("div");
+			square.id = `${x}-${y}`;
+			board.appendChild(square);
+		}
+	}
 
-  drawSnake(board)
+	drawApple();
+	drawSnake(board);
 
-  return board
+	return board;
 }
 
-const board = createBoard()
+const board = createBoard();
 
 function drawSnake(board: HTMLElement) {
-  Array.from(board.children).forEach((square) =>
-    square.classList.remove("snake-square")
-  )
-  snake.forEach((coord) => {
-    const id = coordToId(coord)
-    const square = document.getElementById(id) as HTMLDivElement
-    square.classList.add("snake-square")
-  })
+	Array.from(board.children).forEach((square) =>
+		square.classList.remove("snake-square")
+	);
+	snake.forEach((coord) => {
+		const id = coordToId(coord);
+		const square = document.getElementById(id) as HTMLDivElement;
+		square.classList.add("snake-square");
+	});
 }
 
 function updateSnake() {
-  snake.pop()
-  const [xCurrentHead, yCurrentHead] = snake[0]
-  const [dX, dY] = directionalChange[currentDirection]
+	snake.pop();
+	const [xCurrentHead, yCurrentHead] = snake[0];
+	const [dX, dY] = directionalChange[currentDirection];
 
-  const newHead = [xCurrentHead + dX, yCurrentHead + dY] as Coordinate
-  snake.unshift(newHead)
+	const newHead = [xCurrentHead + dX, yCurrentHead + dY] as Coordinate;
+	snake.unshift(newHead);
 
-  drawSnake(board)
+	drawSnake(board);
+}
+
+function drawApple() {
+	let appleExistsOnSnake: boolean;
+
+	do {
+		apple = [
+			Math.floor(Math.random() * rows),
+			Math.floor(Math.random() * columns),
+		];
+
+		appleExistsOnSnake = snake.some((snakeCoord) =>
+			coordinatesEqual(snakeCoord, apple)
+		);
+	} while (appleExistsOnSnake);
+
+	const appleId = coordToId(apple);
+	const appleSquare = document.getElementById(appleId) as HTMLDivElement;
+	appleSquare.classList.add("apple-square");
 }
 
 document.addEventListener("keydown", (event) => {
-  switch (event.key) {
-    case "ArrowLeft": {
-      if (currentDirection === "right") return
-      currentDirection = "left"
-      break
-    }
-    case "ArrowRight": {
-      if (currentDirection === "left") return
-      currentDirection = "right"
-      break
-    }
-    case "ArrowUp": {
-      if (currentDirection === "down") return
-      currentDirection = "up"
-      break
-    }
-    case "ArrowDown": {
-      if (currentDirection === "up") return
-      currentDirection = "down"
-      break
-    }
-    default: {
-      return
-    }
-  }
-  updateSnake()
-})
+	switch (event.key) {
+		case "ArrowLeft": {
+			if (currentDirection === "right") return;
+			currentDirection = "left";
+			break;
+		}
+		case "ArrowRight": {
+			if (currentDirection === "left") return;
+			currentDirection = "right";
+			break;
+		}
+		case "ArrowUp": {
+			if (currentDirection === "down") return;
+			currentDirection = "up";
+			break;
+		}
+		case "ArrowDown": {
+			if (currentDirection === "up") return;
+			currentDirection = "down";
+			break;
+		}
+		default: {
+			return;
+		}
+	}
+	updateSnake();
+});
--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,23 @@
-import { Coordinate, Direction, Id } from "./main"
+import { Coordinate, Direction, Id } from "./main";
 
 export function idToCoord(id: Id) {
-  return id.split("-").map((el) => parseInt(el)) as Coordinate
+	return id.split("-").map((el) => parseInt(el)) as Coordinate;
 }
 
 export function coordToId([x, y]: Coordinate) {
-  return `${x}-${y}` as Id
+	return `${x}-${y}` as Id;
 }
 
 export const directionalChange: Record<Direction, Coordinate> = {
-  left: [-1, 0],
-  right: [1, 0],
-  up: [0, -1],
-  down: [0, 1],
+	left: [-1, 0],
+	right: [1, 0],
+	up: [0, -1],
+	down: [0, 1],
+};
+
+export function coordinatesEqual(
+	[ax, ay]: Coordinate,
+	[bx, by]: Coordinate
+): boolean {
+	return ax === bx && ay === by;
 }
